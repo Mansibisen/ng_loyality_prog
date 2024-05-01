c2d4cc50b0b6b32c8b91d9fedfcc6e2b9df4901a
mansi <mansibisen354@gmail.com>
2024-05-01 20:33:15 +0530

    added models and repo

---
+++ src/main/java/com/Amex/NextGenLoyality/Repository/TransactionRepository.java
@@ -0,0 +1,15 @@
+package com.Amex.NextGenLoyality.Repository;
+
+
+
+import com.Amex.NextGenLoyality.Models.User;
+import org.springframework.data.repository.CrudRepository;
+
+
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface TransactionRepository extends CrudRepository<User, Integer> {
+
+}
