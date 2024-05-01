866dd6aaeab0bef327208e611eeb5ec9450a3879
mansi <mansibisen354@gmail.com>
2024-05-01 13:18:47 +0530

    starting basic git server

---
+++ src/main/java/com/Amex/NextGenLoyality/NextGenLoyalityApplication.java
@@ -0,0 +1,13 @@
+package com.Amex.NextGenLoyality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextGenLoyalityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextGenLoyalityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Amex/NextGenLoyality/NextGenLoyalityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Amex.NextGenLoyality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextGenLoyalityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
