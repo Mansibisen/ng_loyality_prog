a30549960c39d589913b90ade4566cb8832be0d4
Supriya Prasad <supriya@Supriyas-MacBook-Air.local>
2024-05-04 10:28:51 +0530

    Added transaction and customer model

---
+++ src/main/java/com/Amex/NextGenLoyality/Models/Customer.java
@@ -0,0 +1,73 @@
+package com.Amex.NextGenLoyality.Models;
+
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private String customer_id;
+	private boolean attrition_flag;  // 1 if account is closed else 0
+	private String name;
+	private String email;
+	private String phone_number;
+	private String credit_card_number;
+	private int age;  
+	private boolean gender; // 0 for male, 1 for female
+	
+	public String getCustomer_id() {
+		return customer_id;
+	}
+	public void setCustomer_id(String customer_id) {
+		this.customer_id = customer_id;
+	}
+	public boolean isAttrition_flag() {
+		return attrition_flag;
+	}
+	public void setAttrition_flag(boolean attrition_flag) {
+		this.attrition_flag = attrition_flag;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	public String getCredit_card_number() {
+		return credit_card_number;
+	}
+	public void setCredit_card_number(String credit_card_number) {
+		this.credit_card_number = credit_card_number;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public boolean isGender() {
+		return gender;
+	}
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+
+}
